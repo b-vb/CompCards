8eb991dc1801fc8426ad82ae85256ecfa4cbf73a
Bjorn van Beek <bjorn.vanbeek@people-print.com>
2017-12-03 19:30:07 +0100

    :sparkles: Added elements crud operations

--- routes/elementsRouter.js
+++ routes/elementsRouter.js
@@ -10,15 +10,62 @@ router.use(function (req, res, next) {
     next();
 });
 
+//Get all elements from database
 router.get('/', function (req, res, next) {
     Elements.findAll().then(elements => {
         res.send(elements)
     })
 })
 
+//Get element by Id from database
 router.get('/:elementId', function (req, res, next) {
     Elements.findById(req.params.elementId).then(elements => {
         res.send(elements)
+    })
+})
+
+//Save element in database
+router.post('/new', function (req, res, next) {
+    const newElement = Elements.create({
+        name_en: req.body.name_en,
+        name_nl: req.body.name_nl,
+        fig_code: req.body.fig_code,
+        diff_trampoline: req.body.diff_trampoline,
+        diff_dmt: req.body.diff_dmt
+    }).then(
+        res.sendStatus(200)
+    ).catch(err => {
+        console.error(err);
+    })
+})
+
+//Update element in database
+router.put('/:elementId', function (req, res, next) {
+    Elements.findById(req.params.elementId).then(element => {
+        element.update({
+            name_en: req.body.name_en,
+            name_nl: req.body.name_nl,
+            fig_code: req.body.fig_code,
+            diff_trampoline: req.body.diff_trampoline,
+            diff_dmt: req.body.diff_dmt
+        }).then(
+            res.sendStatus(200)
+        ).catch(err => {
+            console.error(err);
+        })
+    })
+})
+
+//Delete element from database
+router.delete('/:elementId', function (req, res, next) {
+    Elements.destroy({
+        where: {
+            element_id: req.params.elementId
+        }
+    }).then(element => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.log(err);
     })
 })
 
